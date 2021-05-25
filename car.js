7701a43d1cd57c1adb02e41032ae4246f2c57813
VladUgarov <69145359+VladUgarov@users.noreply.github.com>
2021-05-25 16:51:57 +0300

    Update car.js

--- car.js
+++ car.js
@@ -12,54 +12,54 @@ class Car {
     constructor(brand, model, yearOfManufacturing, maxSpeed, maxFuelVolume, fuelConsumption) {
         if (typeof brand === "string" && brand.length >= 1 && brand.length <= 50) {
             this.#brand = brand
-        }
+        }else throw new Error("Брэнд указан неправильно")
         if (typeof model === "string" && model.length >= 1 && model.length <= 50) {
             this.#model = model
-        }
+        }else throw new Error("Модель указана неправильно")
         if (yearOfManufacturing % 1 === 0 && typeof yearOfManufacturing === "number" && isFinite(yearOfManufacturing) && yearOfManufacturing >= 1900 && yearOfManufacturing <= new Date().getFullYear()) {
             this.#yearOfManufacturing = yearOfManufacturing
-        }
+        }else throw new Error("Год указан неправильно")
         if (typeof maxSpeed === "number" && isFinite(maxSpeed) && maxSpeed >= 100 && maxSpeed <= 300) {
             this.#maxSpeed = maxSpeed
-        }
+        }else throw new Error("Максимальная скорость указана неправильно")
         if (typeof maxFuelVolume === "number" && isFinite(maxFuelVolume) && maxFuelVolume >= 5 && maxFuelVolume <= 20) {
             this.#maxFuelVolume = maxFuelVolume
-        }
+        }else throw new Error("Максимальный объем бака указан неправильно")
         if (typeof fuelConsumption === "number" && isFinite(fuelConsumption) && fuelConsumption >= 1.2 && fuelConsumption <= 3) {
             this.#fuelConsumption = fuelConsumption
-        }
+        }else throw new Error("Расход указан неправильно")
 
     }
 
     set brand(brand) {
         if (typeof brand === "string" && brand.length >= 1 && brand.length <= 50) {
             this.#brand = brand
-        }
+        }else throw new Error("Брэнд указан неправильно")
     }
     set model(model) {
         if (typeof model === "string" && model.length >= 1 && model.length <= 50) {
             this.#model = model
-        }
+        }else throw new Error("Модель указана неправильно")
     }
     set yearOfManufacturing(yearOfManufacturing) {
         if (yearOfManufacturing % 1 === 0 && typeof yearOfManufacturing === "number" && isFinite(yearOfManufacturing) && yearOfManufacturing >= 1900 && yearOfManufacturing <= new Date().getFullYear()) {
             this.#yearOfManufacturing = yearOfManufacturing
-        }
+        }else throw new Error("Год указан неправильно")
     }
     set maxSpeed(maxSpeed) {
         if (typeof maxSpeed === "number" && isFinite(maxSpeed) && maxSpeed >= 100 && maxSpeed <= 300) {
             this.#maxSpeed = maxSpeed
-        }
+        }else throw new Error("Максимальная скорость указана неправильно")
     }
     set maxFuelVolume(maxFuelVolume) {
         if (typeof maxFuelVolume === "number" && isFinite(maxFuelVolume) && maxFuelVolume >= 5 && maxFuelVolume <= 20) {
             this.#maxFuelVolume = maxFuelVolume
-        }
+        }else throw new Error("Максимальный объем бака указан неправильно")
     }
     set fuelConsumption(fuelConsumption) {
         if (typeof fuelConsumption === "number" && isFinite(fuelConsumption) && fuelConsumption >= 1.2 && fuelConsumption <= 3) {
             this.#fuelConsumption = fuelConsumption
-        }
+        }else throw new Error("Расход указан неправильно")
     }
 
     get brand() {
@@ -131,4 +131,3 @@ class Car {
     }
 }
 module.exports = Car;
-
